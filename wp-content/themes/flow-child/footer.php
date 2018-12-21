e92858183ad4aff5c0f20709d66cb62a267e7c6e
Samz851 <sam.otb@hotmail.ca>
2018-12-21 04:37:34 -0500

    footer mantis code

---
+++ wp-content/themes/flow-child/footer.php
@@ -0,0 +1,11 @@
+<?php
+flow_elated_get_footer();
+?>
+<script type="text/javascript">
+	var mantis = mantis || [];
+	mantis.push(['display', 'load', {
+		property: '5ac780b4468ff80b1ef64ed1'
+	}]);
+</script>
+
+<script type="text/javascript" data-cfasync="false" src="https://assets.mantisadnetwork.com/mantodea.min.js" async></script>
