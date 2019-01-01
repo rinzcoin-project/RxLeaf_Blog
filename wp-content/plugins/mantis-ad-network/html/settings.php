db1fc74392d38418954ea3645e20eab5d6c5bfae
Samz851 <sam.otb@hotmail.ca>
2018-12-31 23:10:20 -0500

    mantis and team-members

---
+++ wp-content/plugins/mantis-ad-network/html/settings.php
@@ -0,0 +1,69 @@
+<div class="wrap">
+	<h2>MANTIS configuration</h2>
+
+	<form method="post" action="options.php">
+		<?php settings_fields( 'mantis-settings' ); ?>
+		
+		<table class="form-table">
+			<tr valign="top">
+				<th scope="row">Website Identifier:</th>
+				<td>
+					<input type="text" name="mantis_site_id" value="<?php echo get_option('mantis_site_id'); ?>" />
+					<p class="description">This identifier can be found in the code tab of your <a href="https://admin.mantisadnetwork.com" target="_blank">MANTIS administrative panel.</a></p>
+				</td>
+			</tr>
+
+			<tr valign="top">
+				<th scope="row">Load Advertisement Code Asynchronously</th>
+				<td>
+					<select name="mantis_async">
+						<option value="0">Turn Off</option>
+						<option value="1" <?php echo get_option('mantis_async') ? 'selected="selected"' : ''; ?>>Turn On</option>
+					</select>
+					<p class="description">When enabled, the loading of MANTIS advertisements will not impact the time it takes to load a page. However, you may notice a slight delay in serving ads with this option enabled. DO NOT turn on if you use services like CloudFlare.</a></p>
+				</td>
+			</tr>
+
+			<tr valign="top">
+				<th scope="row">Include Javascript at all times</th>
+				<td>
+					<select name="mantis_always">
+						<option value="0">Turn Off</option>
+						<option value="1" <?php echo get_option('mantis_always') ? 'selected="selected"' : ''; ?>>Turn On</option>
+					</select>
+					<p class="description">When enabled, the MANTIS javascript library will always load in the footer. Enable this if you added custom zone tags to your theme.</a></p>
+				</td>
+			</tr>
+
+			<tr valign="top">
+                <th scope="row">After Content Widget</th>
+                <td>
+                    <select name="mantis_after">
+                    	<option value="">Turn Off</option>
+                        <option value="after_content" <?php echo get_option('mantis_after') == 'after_content' ? 'selected="selected"' : ''; ?>>Show After Content</option>
+                        <option value="before_comments" <?php echo get_option('mantis_after') == 'before_comments' ? 'selected="selected"' : ''; ?>>Show Before Comments</option>
+                        <option value="after_comments" <?php echo get_option('mantis_after') == 'after_comments' ? 'selected="selected"' : ''; ?>>Show After Comments</option>
+                    </select>
+                    <p class="description">When enabled, a new area is added to your Appearance > Widget page allowing you to add additional zones using the MANTIS advertisement widget.</a></p>
+                </td>
+            </tr>
+
+            <tr valign="top">
+                <th scope="row">Recommended Content Widget</th>
+                <td>
+                    <select name="mantis_recommend">
+                        <option value="">Turn Off</option>
+                        <option value="after_content" <?php echo get_option('mantis_recommend') == 'after_content' ? 'selected="selected"' : ''; ?>>Show After Content</option>
+                        <option value="before_comments" <?php echo get_option('mantis_recommend') == 'before_comments' ? 'selected="selected"' : ''; ?>>Show Before Comments</option>
+                        <option value="after_comments" <?php echo get_option('mantis_recommend') == 'after_comments' ? 'selected="selected"' : ''; ?>>Show After Comments</option>
+                    </select>
+                    <p class="description">When enabled, the MANTIS content recommendation widget will display after your post content.</a></p>
+                </td>
+            </tr>
+		</table>
+
+		<?php submit_button(); ?>
+	</form>
+</div>
+
+
---
+++ wp-content/plugins/mantis-ad-network/mantis.php
@@ -0,0 +1,115 @@
+<?php
+/*
+Plugin Name: Mantis Ad Network
+Plugin URI: http://wordpress.org/extend/plugins/mantis-ad-network/
+Description: Easily serve advertisements from the Mantis Ad Network on your website.
+Version: 1.5.6
+Author: Mantis Ad Network
+Author URI: http://www.mantisadnetwork.com
+Author Email: contact@mantisadnetwork.com
+License:
+
+	The MIT License (MIT)
+
+	Copyright (c) 2014 Mantis Ad Network <contact@mantisadnetwork.com>
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+*/
+
+define('MANTIS_ROOT', dirname(__FILE__));
+
+require_once(MANTIS_ROOT . '/admin.php');
+require_once(MANTIS_ROOT . '/widget.php');
+require_once(MANTIS_ROOT . '/recommend.php');
+require_once(MANTIS_ROOT . '/after.php');
+
+function mantis_always_footer()
+{
+	if (get_option('mantis_always')) {
+		if (!has_action('wp_footer', 'mantis_ad_footer')) {
+			add_action('wp_footer', 'mantis_ad_footer', 20);
+		}
+	}
+}
+
+add_action('init', 'mantis_always_footer');
+
+/**
+ * Action is registered as wp_footer if at least one advertisement is on the page
+ */
+function mantis_ad_footer()
+{	
+	$site = get_option('mantis_site_id');
+
+	if (!$site) {
+		return;
+	}
+
+	require(dirname(__FILE__) . '/html/config.php');
+
+	require(dirname(__FILE__) . '/html/styling.php');
+
+	if (get_option('mantis_async')) {
+		require(dirname(__FILE__) . '/html/async.html');
+	} else {
+		require(dirname(__FILE__) . '/html/sync.html');
+	}
+}
+
+function mantis_oembed_fetch($provider, $url){
+	 $site = get_option('mantis_site_id');
+
+	if($site && strpos($url, 'mantis.video') !== false){
+	    return add_query_arg( 'property', $site, $provider );
+	}
+
+	return $provider;
+}
+
+function mantis_oembed(){
+    add_filter('oembed_fetch_url', 'mantis_oembed_fetch', 10, 2);
+
+    wp_oembed_add_provider( '#https?://(www\.)?mantis\.video/videos/.*#i', 'http://mantodea.mantisadnetwork.com/video/oembed', true );
+}
+
+add_action('init', 'mantis_oembed');
+
+$mantis_shortcodes = 0;
+
+function mantis_video_shortcode($attrs){
+	global $mantis_shortcodes;
+
+	ob_start();
+
+	extract($attrs);
+
+	$mantis_shortcodes++;
+
+	$property = get_option('mantis_site_id');
+
+	require(dirname(__FILE__) . '/html/video.php');
+
+	$html = ob_get_contents();
+
+	ob_end_clean();
+
+	return $html;
+}
+
+add_shortcode( 'mantis_video', 'mantis_video_shortcode' );
